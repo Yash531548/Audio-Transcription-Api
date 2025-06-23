b840aa973c4b4cc95939ca667470cabfea778560
Yash531548 <yashsharma531548@gmail.com>
2025-06-23 22:39:19 +0530

    Error Handler

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import audioRouter from "./routes/audio.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser());
+
 // Routes 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/audios', audioRouter);
+
+app.use(errorMiddleware);
 
 app.get('/',(req,res)=>{
     res.send('Welcome to Audio Transcription API');
